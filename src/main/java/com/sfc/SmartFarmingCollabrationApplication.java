3f297f2b2475e98263bf00a7e5b99dad8e766d8f
Ashwani chandel <ashwanichandel93964@gmail.com>
2025-10-20 21:39:51 +0530

    Initial commit - Smart Farming Collaboration project

---
+++ src/main/java/com/sfc/SmartFarmingCollabrationApplication.java
@@ -0,0 +1,13 @@
+package com.sfc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFarmingCollabrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFarmingCollabrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sfc/SmartFarmingCollabrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFarmingCollabrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
